5a202547afbced8cc0273f7d0844f1b5e4a174f2
sara <sara.tiffin@centerfiredigital.com>
2025-09-28 14:13:35 -0500

    update sveltekit, add daisyui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="mx-auto flex max-w-2xl flex-col justify-center px-4">
+	<header>
+		<a href="/" class="btn">Home</a>
+		<a href="/projects" class="btn">Projects</a>
+		<a href="https://github.com/sjtiffin" target="_blank" class="btn">Github</a>
+		<a href="https://linkedin.com/in/sjtiffin" target="_blank" class="btn">LinkedIn</a>
+	</header>
+	{@render children?.()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
